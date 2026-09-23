cc92fcc86415f69a509c422df0d5ddb764aedbbb
IamJadviga <gorevanastya1999@gmail.com>
2026-09-23 13:07:39 +0300

    add cels to fars in converter new text

--- converter.py
+++ converter.py
@@ -5,6 +5,10 @@ def km_to_m(km):
 def tonn_to_kg(tonn):
     return tonn * 1000
 
+def cels_to_fars(cels):
+    return (cels * 9/5) + 32
+
 if __name__ == "_main_":
     print(f"10 km = {km_to_m(10)}")
-    print(F"10 tonn = {tonn_to_kg(10)}")
+    print(f"10 tonn = {tonn_to_kg(10)}")
+    print(f"10 cels = {cels_to_fars(10)}")
