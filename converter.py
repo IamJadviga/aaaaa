794e437db4de22602084f49ed9c1d397a0a78e49
IamJadviga <gorevanastya1999@gmail.com>
2026-09-23 13:06:33 +0300

    add tonn to kg in converter

--- converter.py
+++ converter.py
@@ -1,5 +1,10 @@
 def km_to_m(km):
     return km * 1000
 
+
+def tonn_to_kg(tonn):
+    return tonn * 1000
+
 if __name__ == "_main_":
     print(f"10 km = {km_to_m(10)}")
+    print(F"10 tonn = {tonn_to_kg(10)}")
